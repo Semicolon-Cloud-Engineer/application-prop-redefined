2c0847bba2c81f77ceb2f83e40c6f991e22ea81e
Adeola Adekunle <110978314+RevEmmanuel@users.noreply.github.com>
2024-07-19 13:19:53 +0100

    added test script for QA, and added variable for interservice communication  (#29)
    * Initial commit
    
    * Initial commit
    
    * Yaml file update
    
    * Update application.yml
    
    * Create merge-systest.yml
    
    * Update merge-systest.yml
    
    * Update merge-systest.yml
    
    * Delete .github/workflows/merge-systest.yml
    
    * Update and rename build-create-docker-image.yml to build-and-deploy.yml
    
    * Update build-and-deploy.yml
    
    * Update build-and-deploy.yml
    
    * Update build-and-deploy.yml
    
    * Update build-and-deploy.yml
    
    * Update build-and-deploy.yml
    
    * Create notify.yml
    
    * Update notify.yml
    
    * Update notify.yml
    
    * Delete .github/workflows/notify.yml
    
    * Create build-notify-deploy.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-create-docker-image.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-create-docker-image.yml
    
    * Update build-notify-deploy.yml
    
    * Updated build-create-docker-image.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-notify-deploy.yml
    
    * Update build-notify-deploy.yml
    
    * Update README.md
    
    * Update docker-compose.yml
    
    * Update docker-compose.yml
    
    * Update application.yml
    
    * First Identity Verification Commit
    
    * Commiting gitignore changes
    
    * abstracted some exception messages
    
    * docker compose removed and keycloak added
    
    * clean up branch
    
    * entities and flyway
    
    * progress on invite merchant acquirer
    
    * modified merchant acquirer to fit for all organizations
    
    * added other aspects of invite organization
    
    * controller and finished with organization, employee left
    
    * skeleton for employee
    
    * employee setup remains business party
    
    * added business party
    
    * added employee domain object
    
    * unit tests
    
    * organizations
    
    * added test cases
    
    * finished test cases for invite merchant acquirer
    
    * exception handler, swagger ui
    
    * updated gitignore to remove pulsar and rabbitmq data
    
    * edited structure
    
    * reporting and properties change
    
    * changes
    
    * profile for test
    
    * port update
    
    * fixes
    
    * updated sql script for flyway
    
    * removed test profile
    
    * removed test profile reference
    
    * mongodb changes
    
    * renamed all to properties file and parameterized variables
    
    * updated db connection details
    
    * updated variables
    
    * added port and name to application properties
    
    * reconfigured eureka
    
    * fixed eureka url
    
    * set prefer ip to true
    
    * register with eureka
    
    * aded exception for multiple client
    
    * added qa plugin and script
    
    * added eureka instance hostname variable
    
    ---------
    
    Co-authored-by: Joshua Oluwakuse <117924389+Joshrichhy@users.noreply.github.com>
    Co-authored-by: ernest-emmanuel-utibe <em123nest@gmail.com>
    Co-authored-by: Ezugwuugwu <ezugwuugwu@gmial.com>

--- src/main/java/com/example/OnboardingIdentityManagementService/infrastructure/adapters/input/rest/utils/constants/ResponseConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.OnboardingIdentityManagementService.infrastructure.adapters.input.rest.utils.constants;
-
-public class ResponseConstants {
-
-    public static final String SUCCESS_MESSAGE = "SUCCESS!";
-
-}
--- src/main/java/com/example/OnboardingIdentityManagementService/infrastructure/adapters/input/rest/utils/constants/UrlConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.OnboardingIdentityManagementService.infrastructure.adapters.input.rest.utils.constants;
-
-public class UrlConstants {
-
-    public static final String BASE_URL = "/api/v1/identity";
-
-}
