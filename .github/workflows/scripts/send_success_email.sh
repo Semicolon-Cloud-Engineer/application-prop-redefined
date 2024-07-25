c6dc9f9cfe56a608a0374534042160a6e008431b
Joshua Oluwakuse <117924389+Joshrichhy@users.noreply.github.com>
2024-07-25 22:29:52 +0100

    Update send_success_email.sh (#102)

--- .github/workflows/scripts/send_success_email.sh
+++ .github/workflows/scripts/send_success_email.sh
@@ -16,15 +16,14 @@ do
   echo "To: $email" >> /tmp/email.txt
   echo "Subject: Build Report" >> /tmp/email.txt
   printf "Congratulations, your recent build in Identity Management Backend was successful.\nTAG: ${TAG}" >> /tmp/email.txt
-  printf "Branch: ${BRANCH_NAME}\n" >> /tmp/email.txt
+  printf "\nBranch: ${BRANCH_NAME}\n" >> /tmp/email.txt
   printf "Author: ${COMMIT_AUTHOR}\n" >> /tmp/email.txt
-  printf "\nTAG: ${TAG}\n\n" >> /tmp/email.txt
-  printf "click on the links below to view your reports\n" >> /tmp/email.txt
-  printf "Maven Build Report: https://semicolon-build-reports.s3.eu-west-1.amazonaws.com/karrabo/identity-management/maven-reports/new-reports/index.html\n" >> /tmp/email.txt
-  printf "Automation Test Report: https://semicolon-build-reports.s3.eu-west-1.amazonaws.com/karrabo/identity-management/automation-tests-result/report-pytest-results.html\n" >> /tmp/email.txt
-  printf "Sonarqube Report: http://sonarqube.enum.africa/dashboard?id=Karrabo-Identity-Management\n" >> /tmp/email.txt
+  printf "\nclick on the links below to view your reports\n" >> /tmp/email.txt
+  printf "\n\nMaven Build Report: https://semicolon-build-reports.s3.eu-west-1.amazonaws.com/karrabo/identity-management/maven-reports/new-reports/index.html\n" >> /tmp/email.txt
+  printf "\n\nAutomation Test Report: https://semicolon-build-reports.s3.eu-west-1.amazonaws.com/karrabo/identity-management/automation-tests-result/report-pytest-results.html\n" >> /tmp/email.txt
+  printf "\n\nSonarqube Report: http://sonarqube.enum.africa/dashboard?id=Karrabo-Identity-Management\n" >> /tmp/email.txt
   
-  printf "\nRegards,\nThe Cloud Team" >> /tmp/email.txt
+  printf "\n\n\nRegards,\nThe Cloud Team" >> /tmp/email.txt
   curl --ssl-reqd \
     --url "smtps://${SMTP_SERVER}:${SMTP_PORT}" \
     --mail-from "builds@semicolon.africa" \
