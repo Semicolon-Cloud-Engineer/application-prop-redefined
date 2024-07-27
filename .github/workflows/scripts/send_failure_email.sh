11e0e6e421f1c019e70c103a0dcb8d35148d0ff4
Joshua Oluwakuse <117924389+Joshrichhy@users.noreply.github.com>
2024-07-27 17:08:30 +0100

    Update send_failure_email.sh (#134)

--- .github/workflows/scripts/send_failure_email.sh
+++ .github/workflows/scripts/send_failure_email.sh
@@ -7,9 +7,10 @@ EMAILS=$5
 TAG=$6
 BRANCH_NAME=$7
 COMMIT_AUTHOR=${8}
-SONARQUBE_URL=${9}
-AUTOMATION_TEST_URL=${10}
-MAVEN_REPORT_URL=${11}
+SONARQUBE_URL=http://sonarqube.enum.africa/dashboard?id=Karrabo-Identity-Management
+MAVEN_REPORT_URL=https://semicolon-build-reports.s3.eu-west-1.amazonaws.com/karrabo/identity-management/maven-reports/new-reports/index.html
+AUTOMATION_TEST_URL=https://semicolon-build-reports.s3.eu-west-1.amazonaws.com/karrabo/identity-management/automation-tests-result/report-pytest-results.html
+      
 SONARQUBE_URL_SET=${12}
 MAVEN_REPORT_URL_SET=${13}
 AUTOMATION_TEST_URL_SET=${14}
