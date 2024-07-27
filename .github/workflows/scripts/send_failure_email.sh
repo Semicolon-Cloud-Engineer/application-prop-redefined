b6f2244440170299b5169b36c15a11621a8bf70b
Joshua Oluwakuse <117924389+Joshrichhy@users.noreply.github.com>
2024-07-27 15:18:34 +0100

    Update send_failure_email.sh (#123)

--- .github/workflows/scripts/send_failure_email.sh
+++ .github/workflows/scripts/send_failure_email.sh
@@ -7,44 +7,38 @@ SMTP_PASSWORD=$4
 EMAILS=$5
 TAG=$6
 BRANCH_NAME=$7
-COMMIT_AUTHOR=${10}
-SONARQUBE_URL=${11}
-AUTOMATION_TEST_URL=${12} 
-MAVEN_REPORT_URL=${13}
+COMMIT_AUTHOR=${8}
+SONARQUBE_URL=${9}
+AUTOMATION_TEST_URL=${10}
+MAVEN_REPORT_URL=${11}
 
-
-
-        IFS=',' read -r -a email_array <<< "\${EMAILS}"
-        for email in "\${email_array[@]}"
-        do
-          echo "From: builds@semicolon.africa" > /tmp/email.txt
-          echo "To: \$email" >> /tmp/email.txt
-          echo "Subject: Build Failure" >> /tmp/email.txt
-          printf "Oooops, Your recent build in Identity Management Backend was unsuccessful, kindly check the new commit and fix.\n\n" >> /tmp/email.txt
-          printf "Branch: \${BRANCH_NAME}\n" >> /tmp/email.txt
-          printf "Author: \${COMMIT_AUTHOR}\n" >> /tmp/email.txt
-          printf "\nTAG: \${TAG}\n\n" >> /tmp/email.txt
-          printf "click on the links below to view your reports\n" >> /tmp/email.txt
-          
-          if [ -n "\$SONARQUBE_URL" ]; then
-            printf "\nBuild and Analyze Job: \$SONARQUBE_URL\n" >> /tmp/email.txt
-          fi
-          if [ -n "\$MAVEN_REPORT_URL" ]; then
-            printf "\nDeploy and Run Job: \$MAVEN_REPORT_URL\n" >> /tmp/email.txt
-          fi
-          if [ -n "\$AUTOMATION_TEST_URL" ]; then
-            printf "\nAutomation Test Job: \$AUTOMATION_TEST_URL\n" >> /tmp/email.txt
-          fi
-
-          # printf "\n\nMaven Build Report: https://semicolon-build-reports.s3.eu-west-1.amazonaws.com/karrabo/identity-management/maven-reports/new-reports/index.html\n" >> /tmp/email.txt
-          # printf "\n\nAutomation Test Report: https://semicolon-build-reports.s3.eu-west-1.amazonaws.com/karrabo/identity-management/automation-tests-result/report-pytest-results.html\n" >> /tmp/email.txt
-          # printf "\n\nSonarqube Report: http://sonarqube.enum.africa/dashboard?id=Karrabo-Identity-Management\n\n" >> /tmp/email.txt
-          
-          printf "\n\nRegards,\nThe Cloud Team" >> /tmp/email.txt
-          curl --ssl-reqd \
-            --url "smtps://\${SMTP_SERVER}:\${SMTP_PORT}" \
-            --mail-from "builds@semicolon.africa" \
-            --mail-rcpt "\$email" \
-            --user "\${SMTP_USERNAME}:\${SMTP_PASSWORD}" \
-            --upload-file /tmp/email.txt
-        done
+IFS=',' read -r -a email_array <<< "${EMAILS}"
+for email in "${email_array[@]}"
+do
+  echo "From: builds@semicolon.africa" > /tmp/email.txt
+  echo "To: $email" >> /tmp/email.txt
+  echo "Subject: Build Failure" >> /tmp/email.txt
+  printf "Oooops, Your recent build in Identity Management Backend was unsuccessful, kindly check the new commit and fix.\n\n" >> /tmp/email.txt
+  printf "Branch: ${BRANCH_NAME}\n" >> /tmp/email.txt
+  printf "Author: ${COMMIT_AUTHOR}\n" >> /tmp/email.txt
+  printf "\nTAG: ${TAG}\n\n" >> /tmp/email.txt
+  printf "Click on the links below to view your reports\n" >> /tmp/email.txt
+  
+  if [ -n "$SONARQUBE_URL" ]; then
+    printf "\nBuild and Analyze Job: $SONARQUBE_URL\n" >> /tmp/email.txt
+  fi
+  if [ -n "$MAVEN_REPORT_URL" ]; then
+    printf "\nDeploy and Run Job: $MAVEN_REPORT_URL\n" >> /tmp/email.txt
+  fi
+  if [ -n "$AUTOMATION_TEST_URL" ]; then
+    printf "\nAutomation Test Job: $AUTOMATION_TEST_URL\n" >> /tmp/email.txt
+  fi
+  
+  printf "\n\nRegards,\nThe Cloud Team" >> /tmp/email.txt
+  curl --ssl-reqd \
+    --url "smtps://${SMTP_SERVER}:${SMTP_PORT}" \
+    --mail-from "builds@semicolon.africa" \
+    --mail-rcpt "$email" \
+    --user "${SMTP_USERNAME}:${SMTP_PASSWORD}" \
+    --upload-file /tmp/email.txt
+done
